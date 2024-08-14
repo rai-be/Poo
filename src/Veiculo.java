83ed77fcf8a79f4e476de39d3a9189866d36e02e
raiani <raiani.guimaraes@zup.com.br>
2024-08-14 16:34:57 -0300

    adding vehicle class changess

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,34 +1,41 @@
 
-    public class Veiculo {
+    public class Veiculo
+{
     // Atributos
     private String marca;
     private String modelo;
     private int ano;
 
-    // Construtor
+    // Metodo Construtor
     public Veiculo(String marca, String modelo, int ano) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
     }
 
-    public Veiculo() {}
+    public Veiculo() {
 
-    // Metodo de acesso
+    }
+
+    // Metodo marca
     public String getMarca() {
         return marca;
     }
 
+    // Metodo modelo
     public String getModelo() {
         return modelo;
     }
 
+    // Metodo ano
     public int getAno() {
         return ano;
     }
-
+    // Metodo toString
     @Override
     public String toString() {
-        return String.format("Marca: %s, Modelo: %s, Ano: %d", getMarca(), getModelo(), getAno());
+        return "Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano;
+
     }
 }
+
